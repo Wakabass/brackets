1dad26db3b23c32d07a7cfc055dff9ed6f2aeb6c
Sforca <invisible_dream@mail.ru>
2019-03-22 15:02:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    let arr = [];
+  for(let i = 0; i < str.length; i++){
+    if(str[i] == '(' || str[i] == '[' || str[i] == '{'){
+      arr.push(str[i]);
+    }
+    else if(str[i] == ')' || str[i] == ']' || str[i] == '}'){
+      arr.pop();
+    }
+  }
+  if(arr.length == 0) {
+    return true
+  }
+  else{
+    return false;
+  }
+
 }
